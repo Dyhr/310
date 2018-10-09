85582e13255a257e91c3eee1352ed7818dcefa49
Rasmus Dyhr Larsen <rasmus.dyhr@gmail.com>
2018-10-09 21:26:07 +0200

    Got an encrypted string. Working on de-noising.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from __future__ import absolute_import, unicode_literals
-import png, array
+import png, array, hashlib
 
 print("Setting up")
 
@@ -7,55 +7,76 @@ reader = png.Reader(filename='challenge.png')
 w, h, pixels, metadata = reader.read_flat()
 
 pixel_byte_width = 4 if metadata['alpha'] else 3
-result_pixels = pixels[:]
+alpha_pixels = pixels[:]
+gray_pixels = pixels[:]
 
 row310 = [0] * w
 
-print("Detecting steganography")
+print("Detecting steganograph")
 
 for i in range(0, len(pixels), pixel_byte_width):
-    if(pixels[i] != pixels[i+1] or pixels[i+1] != pixels[i+2] or pixels[i] != pixels[i+2]):
-        result_pixels[i+0] = 0
-        result_pixels[i+1] = 0
-        result_pixels[i+2] = 0
-
-        x = int((i/pixel_byte_width) % w)
-        y = int((i/pixel_byte_width) / w)
+    x = int((i/pixel_byte_width) % w)
+    y = int((i/pixel_byte_width) / w)
+    pixel = [pixels[i+0], pixels[i+1], pixels[i+2], pixels[i+3]]
+    mask = [
+        (255-pixel[0]) & int('00000000',2),
+        (255-pixel[1]) & int('00000000',2),
+        (255-pixel[2]) & int('00000000',2),
+        (255-pixel[3]) & int('00000001',2),
+    ]
+    
+    if sum(mask) != 0:
+        alpha_pixels[i+0] = 0
+        alpha_pixels[i+1] = 0
+        alpha_pixels[i+2] = 0
+        
         if y == 310:
             row310[x] = 1
+        #print(str(x)+" "+str(y)+" "+bin(diff))
     else:
-        result_pixels[i+0] = 255
-        result_pixels[i+1] = 255
-        result_pixels[i+2] = 255
+        alpha_pixels[i+0] = 255
+        alpha_pixels[i+1] = 255
+        alpha_pixels[i+2] = 255
+
+    if (pixel[0] != 0 and pixel[0] != 255) or (pixel[1] != 0 and pixel[1] != 255) or (pixel[2] != 0 and pixel[2] != 255):
+        gray_pixels[i+0] = pixel[0]
+        gray_pixels[i+1] = pixel[1]
+        gray_pixels[i+2] = pixel[2]
+    else:
+        gray_pixels[i+0] = 0
+        gray_pixels[i+1] = 0
+        gray_pixels[i+2] = 0
 
 
 print("Saving steganography image")
 
 output = open('challenge-2.png', 'wb')
 writer = png.Writer(w, h, **metadata)
-writer.write_array(output, result_pixels)
+writer.write_array(output, alpha_pixels)
 output.close()
 
 
-print("Row 310 bits")
-row310_bytes = []
-for i in range(0, len(row310), 8):
-    row310_bytes.append(int(''.join(str(x) for x in row310[i:i+8]), 2))
-print(row310_bytes)
-
-row310_image = []
-for byte in row310_bytes:
-    row310_image.append(byte)
-    row310_image.append(byte)
-    row310_image.append(byte)
+print("Saving another steganography image")
 
 output = open('challenge-3.png', 'wb')
-writer = png.Writer(16, 23)
-writer.write_array(output, row310_image)
+writer = png.Writer(w, h, **metadata)
+writer.write_array(output, gray_pixels)
 output.close()
 
-# Possible image sizes
-# 1  , 2  , 4 , 8 , 16, 23, 46, 92, 184, 368
-# 368, 184, 92, 46, 23, 16, 8 , 4 , 2  , 1
+
+print("Row 310 message 1:")
+
+row310_bytes = []
+for i in range(0, len(row310), 8):
+    row310_bytes.append(int(''.join(str(1-x) for x in row310[i:i+8]), 2))
+
+row310_message = ''.join([str(chr(b)) if b != 255 else "" for b in row310_bytes])
+
+print(row310_message)
+
+print(hashlib.sha256(row310_message).hexdigest())
+
 
 print("Done")
+
+# L503K7CF64C120E89D9D4
